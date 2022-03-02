aa535baaceeccda9ae633ed097c3a8ef1772b6ea
unknown <mbrector@gmail.com>
2022-03-02 13:43:05 -0500

    Magic 8 Ball express application created

--- server.js
+++ server.js
@@ -24,7 +24,14 @@ app.get('/greeting/:name', (req, res) => {
 })
 
 app.get('/tip/:total/:tipPercentage', (req, res) => {
-  res.render('template', { title: 'Tip', message: 'Your tip should be:', content: req.params.total * (req.params.tipPercentage)/100 })
+  res.render('template', { title: 'Tip', message: 'Your tip should be:', content: req.params.total * (req.params.tipPercentage)/100 }) 
+})
+
+const answer = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+
+app.get('/magic/:question', (req, res) => {
+  const rando = answer[Math.floor(Math.random()*answer.length)]
+  res.render('template', { title: 'Magic 8 Ball', message: req.params.question , content: '<h1>' + rando + '<h1>' })
 })
 
 
